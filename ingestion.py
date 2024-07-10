dfd85a7be817b93a762be8cd9b260e25373a09f8
tech-admins <tech-admins@appsforbharat.com>
2024-07-10 15:35:22 +0530

    Reversed the chat for better UX

--- ingestion.py
+++ ingestion.py
@@ -16,7 +16,7 @@ embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
 
 def ingest_docs():
-    directory = ''  # Specify your directory path here
+    directory = 'sm-docs'  # Specify your directory path here
     html_contents = read_html_files(directory)
     print(f"loaded {len(html_contents)} documents")
 
@@ -51,5 +51,5 @@ def ingest_text_file():
 
 
 if __name__ == "__main__":
-    # ingest_docs()
-    ingest_text_file()
+    ingest_docs()
+    # ingest_text_file()
